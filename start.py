fc247e57455d6a5f27827daf2feb2747c81e76d0
modbots <77435462+modbots@users.noreply.github.com>
2023-02-01 23:23:19 +0630

    Create start.py

---
+++ start.py
@@ -0,0 +1,7 @@
+from flask import *
+import requests
+from subprocess import Popen
+app = Flask(__name__)
+Popen(f"gunicorn server.server:app --bind 0.0.0.0:8080", shell=True)
+if __name__=="__main__":
+    app.run()
